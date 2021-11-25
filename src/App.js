0bd63fcbdccc92068022fc0052acfb743adc87d5
len3fun <len3fun@gmail.com>
2021-11-25 15:11:14 +0300

    App componentes was rewritten with hooks

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import Main from "./components/main";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Signup from "./components/signup";
 
-class App extends Component {
-
-    render() {
-        return (
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Main/>}/>
-                    <Route path="/sign-up" element={<Signup/>}/>
-
-                </Routes>
-
-            </Router>
-        );
-    }
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Main/>}/>
+                <Route path="/sign-up" element={<Signup/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
